d180af2e606bd352405aaa1326df3ad54d2a8184
Hamza Chebbah <hamzachebbah9999@gmail.com>
2023-10-20 23:39:58 +0100

    init repo

---
+++ src/pages/index.tsx
@@ -0,0 +1,23 @@
+import MainLayout from "@/layouts";
+import { NextPageWithLayout } from "@/types";
+import { Search } from "lucide-react";
+import { ReactElement } from "react";
+
+const Home: NextPageWithLayout = () => {
+  return (
+    <section
+      className={`flex min-h-screen flex-col items-center justify-center`}
+    >
+      <h1 className={"text-5xl font-bold"}>MICROBOARD</h1>
+      <p>test ci</p>
+      <p>test discord webhook release</p>
+
+      <Search></Search>
+    </section>
+  );
+};
+
+export default Home;
+Home.getLayout = function getLayout(page: ReactElement) {
+  return <MainLayout>{page}</MainLayout>;
+};
