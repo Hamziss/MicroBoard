8993dca1309880d882898d8ec2415864fcf8b423
Anis <anislaib@outlook.com>
2023-10-27 17:53:32 +0100

    added the navbar component and made the dashboard content page

---
+++ src/components/sidebar.tsx
@@ -0,0 +1,11 @@
+interface SidebarProps extends React.HTMLAttributes<HTMLElement> {
+    // TODO: Add props
+}
+
+export function Sidebar({ className, ...props }: SidebarProps) {
+    return (
+        <>
+            Sidebar
+        </>
+    )
+}
