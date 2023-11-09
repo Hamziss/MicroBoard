8561e6ecaccd2d08d67cb81dbd666051070dccb0
younesfdj <ferradjiyounes69@gmail.com>
2023-11-10 00:18:26 +0100

    Fix: canva navbar components

--- src/components/ui/mainCenter.tsx
+++ src/components/ui/mainCenter.tsx
@@ -1,17 +1,19 @@
 import { MousePointer2, Hand, Square, Circle, Pen, Image, Eraser, MoveRight, Minus } from 'lucide-react'
+import styles from "@/styles/MainComponent.module.css"
+
 const MainCenter = () => {
     return (
-        <div className="flex h-10 w-[33rem] items-center justify-between rounded-md border border-solid border-slate-200 px-4 cursor-pointer hover:shadow-lg">
+        <div className="flex h-10 w-[33rem] items-center justify-between rounded-md border border-solid border-slate-200 px-4 cursor-pointer hover:shadow-lg bg-white">
             <div className='flex justify-between items-center w-full'>
-                <div className='center-icon'><MousePointer2 /></div>
-                <div className='center-icon'><Hand /></div>
-                <div className='center-icon'><Square /></div>
-                <div className='center-icon'><Circle /></div>
-                <div className='center-icon'><Pen /></div>
-                <div className='center-icon'><Image /></div>
-                <div className='center-icon'><Eraser /></div>
-                <div className='center-icon'><MoveRight /></div>
-                <div className='center-icon'><Minus /></div>
+                <div className={styles.centerIcon}><MousePointer2 /></div>
+                <div className={styles.centerIcon}><Hand /></div>
+                <div className={styles.centerIcon}><Square /></div>
+                <div className={styles.centerIcon}><Circle /></div>
+                <div className={styles.centerIcon}><Pen /></div>
+                <div className={styles.centerIcon}><Image /></div>
+                <div className={styles.centerIcon}><Eraser /></div>
+                <div className={styles.centerIcon}><MoveRight /></div>
+                <div className={styles.centerIcon}><Minus /></div>
             </div>
         </div>
     )
